ef5f2f31c242c142e049e6064b5b5493723266cf
preethika <preethikak@adobe.com>
2026-05-06 15:27:32 +0530

    klii

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -426,7 +426,7 @@ function fetchReviewDetailsAPI(globals) {
 function handleProceedAPI(globals) {
  
   const phone =
-    document.querySelector('input[name="mobile"]')?.value || "";
+    document.querySelector('input[name="aadhaar_linked_mobile_number"]')?.value || "";
  
   fetch(" https://writing-dimly-spout.ngrok-free.dev/proceed-details", {
     method: "POST",
